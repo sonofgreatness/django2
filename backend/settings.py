99571ae75d60c7000ee5dd99f6c1ff311541f39a
sonofgreatness <simphiweiq@gmail.com>
2025-03-26 19:38:32 +0200

    initial 3

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0x4m*1-f#bp^c%mu)97(q*_hu3pf**=#ygmq&0&-dwyqvfwp5)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
